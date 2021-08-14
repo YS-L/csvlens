3147b8792f14a14e18ee57a32c031eedef92fd74
Yung Siang Liau <liauys@gmail.com>
2021-08-14 19:19:52 +0800

    Refactor row rendering

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,6 @@ impl<'a> CsvTable<'a> {
         let line_number_block = Block::default()
             .borders(Borders::RIGHT)
             .border_style(Style::default().fg(Color::Rgb(64, 64, 64)));
-        //let line_number_area = Rect::new(0, 0, section_width, area.height);
         let line_number_area = Rect::new(
             0,
             y_first_record,
@@ -113,6 +112,26 @@ impl<'a> CsvTable<'a> {
         block.render(area, buf);
         // y pos of header text and next line
         (height.saturating_sub(2), height)
+    }
+
+    fn render_row(&self, buf: &mut Buffer, column_widths: &[u16], area: Rect, x: u16, y: u16, is_header: bool, row: &Vec<String>) {
+        let mut x_offset_header = x;
+        let mut remaining_width = area.width.saturating_sub(x);
+        for (hname, hlen) in row.iter().zip(column_widths) {
+            // TODO: why all the *hlen deferences; any easier way?
+            if remaining_width < *hlen {
+                break;
+            }
+            let mut style = Style::default();
+            if is_header {
+                style = style.add_modifier(Modifier::BOLD);
+
+            }
+            let span = Span::styled((*hname).as_str(), style);
+            buf.set_span(x_offset_header, y, &span, *hlen);
+            x_offset_header += hlen;
+            remaining_width = remaining_width.saturating_sub(*hlen);
+        }
     }
 }
 
@@ -137,35 +156,27 @@ impl<'a> StatefulWidget for CsvTable<'a> {
             y_first_record,
         );
 
-        let mut x_offset_header = row_num_section_width;
-        let mut remaining_width = area.width.saturating_sub(row_num_section_width);
-        for (hname, hlen) in self.header.iter().zip(&column_widths) {
-            // TODO: why all the *hlen deferences; any easier way?
-            if remaining_width < *hlen {
-                break;
-            }
-            let style = Style::default()
-                .add_modifier(Modifier::BOLD);
-            let span = Span::styled((*hname).as_str(), style);
-            buf.set_span(x_offset_header, y_header, &span, *hlen);
-            x_offset_header += hlen;
-            remaining_width = remaining_width.saturating_sub(*hlen);
-        }
+        self.render_row(
+            buf,
+            &column_widths,
+            area,
+            row_num_section_width,
+            y_header,
+            true,
+            &self.header,
+        );
 
         let mut y_offset = y_first_record;
         for row in self.rows.iter() {
-            // TODO: duplicate with rendering of headers
-            let mut x_offset_header = row_num_section_width;
-            let mut remaining_width = area.width.saturating_sub(row_num_section_width);
-            for (value, hlen) in row.iter().zip(&column_widths) {
-                if remaining_width < *hlen {
-                    break;
-                }
-                let span = Span::from((*value).as_str());
-                buf.set_span(x_offset_header, y_offset, &span, *hlen);
-                x_offset_header += hlen;
-                remaining_width = remaining_width.saturating_sub(*hlen);
-            }
+            self.render_row(
+                buf,
+                &column_widths,
+                area,
+                row_num_section_width,
+                y_offset,
+                false,
+                row,
+            );
             y_offset += 1;
             if y_offset >= area.height {
                 break;
@@ -174,7 +185,9 @@ impl<'a> StatefulWidget for CsvTable<'a> {
     }
 }
 pub struct CsvTableState {
+    // TODO: types appropriate?
     rows_offset: u64,
+    cols_offset: u64,
 }
 
 impl CsvTableState {
@@ -182,6 +195,7 @@ impl CsvTableState {
     fn new() -> Self {
         Self {
             rows_offset: 0,
+            cols_offset: 0,
         }
     }
 
