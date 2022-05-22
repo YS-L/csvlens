56e47250f2e97b61eaf3bdec7b087e33e108a2fb
Yung Siang Liau <liauys@gmail.com>
2022-05-22 12:41:56 +0800

    Refactor main app as struct

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use std::sync::Arc;
 use std::usize;
 use tempfile::NamedTempFile;
 use termion::{raw::IntoRawMode, screen::AlternateScreen};
-use tui::backend::TermionBackend;
+use tui::backend::{TermionBackend, Backend};
 use tui::Terminal;
 
 fn get_offsets_to_make_visible(
@@ -147,6 +147,234 @@ fn parse_delimiter(args: &Args) -> Result<Option<u8>> {
     }
 }
 
+struct App {
+    input_handler: InputHandler,
+    num_rows_not_visible: u16,
+    shared_config: Arc<csv::CsvConfig>,
+    rows_view: view::RowsView,
+    headers: Vec<String>,
+    csv_table_state: CsvTableState,
+    finder: Option<find::Finder>,
+    first_found_scrolled: bool,
+    user_error: Option<String>,
+    show_stats: bool,
+}
+
+impl App {
+
+    fn new(
+        filename: &str,
+        delimiter: Option<u8>,
+        original_filename: Option<String>,
+        show_stats: bool,
+    ) -> Result<Self> {
+        let input_handler = InputHandler::new();
+
+        // Some lines are reserved for plotting headers (3 lines for headers + 2 lines for status bar)
+        let num_rows_not_visible: u16 = 5;
+
+        // Number of rows that are visible in the current frame
+        let num_rows = 50 - num_rows_not_visible;
+
+        let mut config = csv::CsvConfig::new(filename);
+        if let Some(d) = delimiter {
+            config.delimiter = d;
+        }
+        let shared_config = Arc::new(config);
+
+        let csvlens_reader = csv::CsvLensReader::new(shared_config.clone())
+            .context(format!("Failed to open file: {}", filename))?;
+        let rows_view = view::RowsView::new(csvlens_reader, num_rows as u64)?;
+        let headers = rows_view.headers().clone();
+
+        let csv_table_state = CsvTableState::new(
+            original_filename, headers.len()
+        );
+
+        let finder: Option<find::Finder> = None;
+        let first_found_scrolled = false;
+
+        let user_error: Option<String> = None;
+
+        let app = App {
+            input_handler,
+            shared_config,
+            num_rows_not_visible,
+            rows_view,
+            headers,
+            csv_table_state,
+            finder,
+            first_found_scrolled,
+            user_error,
+            show_stats,
+        };
+
+        Ok(app)
+    }
+
+    pub fn main_loop<B: Backend>(&mut self, terminal: &mut Terminal<B>) -> Result<()> {
+        loop {
+            let control = self.input_handler.next();
+            if matches!(control, Control::Quit) {
+                break;
+            }
+            self.step(control, terminal)?;
+        }
+        Ok(())
+    }
+
+    fn step<B: Backend>(&mut self, control: Control, terminal: &mut Terminal<B>) -> Result<()> {
+
+        // clear error message without changing other states on any action
+        if !matches!(control, Control::Nothing) {
+            self.user_error = None;
+        }
+
+        self.rows_view.handle_control(&control)?;
+
+        match &control {
+            Control::ScrollTo(_) => {
+                self.csv_table_state.reset_buffer();
+            }
+            Control::ScrollLeft => {
+                let new_cols_offset = self.csv_table_state.cols_offset.saturating_sub(1);
+                self.csv_table_state.set_cols_offset(new_cols_offset);
+            }
+            Control::ScrollRight => {
+                if self.csv_table_state.has_more_cols_to_show() {
+                    let new_cols_offset = self.csv_table_state.cols_offset.saturating_add(1);
+                    self.csv_table_state.set_cols_offset(new_cols_offset);
+                }
+            }
+            Control::ScrollToNextFound if !self.rows_view.is_filter() => {
+                if let Some(fdr) = self.finder.as_mut() {
+                    if let Some(found_record) = fdr.next() {
+                        scroll_to_found_record(found_record, &mut self.rows_view, &mut self.csv_table_state);
+                    }
+                }
+            }
+            Control::ScrollToPrevFound if !self.rows_view.is_filter() => {
+                if let Some(fdr) = self.finder.as_mut() {
+                    if let Some(found_record) = fdr.prev() {
+                        scroll_to_found_record(found_record, &mut self.rows_view, &mut self.csv_table_state);
+                    }
+                }
+            }
+            Control::Find(s) | Control::Filter(s) => {
+                let re = Regex::new(s.as_str());
+                if let Ok(target) = re {
+                    // TODO: need to reset row views filter if any first?
+                    self.finder = Some(find::Finder::new(self.shared_config.clone(), target).unwrap());
+                    match control {
+                        Control::Find(_) => {
+                            // will scroll to first result below once ready
+                            self.first_found_scrolled = false;
+                            self.rows_view.reset_filter().unwrap();
+                        }
+                        Control::Filter(_) => {
+                            self.rows_view.set_rows_from(0).unwrap();
+                            self.rows_view.set_filter(self.finder.as_ref().unwrap()).unwrap();
+                        }
+                        _ => {}
+                    }
+                } else {
+                    self.finder = None;
+                    // TODO: how to show multi-line error
+                    self.user_error = Some(format!("Invalid regex: {}", s));
+                }
+                self.csv_table_state.reset_buffer();
+            }
+            Control::BufferContent(buf) => {
+                self.csv_table_state.set_buffer(self.input_handler.mode(), buf.as_str());
+            }
+            Control::BufferReset => {
+                self.csv_table_state.reset_buffer();
+                if self.finder.is_some() {
+                    self.finder = None;
+                    self.csv_table_state.finder_state = FinderState::FinderInactive;
+                    self.rows_view.reset_filter().unwrap();
+                }
+            }
+            _ => {}
+        }
+
+        if let Some(fdr) = self.finder.as_mut() {
+            if !self.rows_view.is_filter() {
+                // scroll to first result once ready
+                if !self.first_found_scrolled && fdr.count() > 0 {
+                    // set row_hint to 0 so that this always scrolls to first result
+                    fdr.set_row_hint(0);
+                    if let Some(found_record) = fdr.next() {
+                        scroll_to_found_record(found_record, &mut self.rows_view, &mut self.csv_table_state);
+                    }
+                    self.first_found_scrolled = true;
+                }
+
+                // reset cursor if out of view
+                if let Some(cursor_row_index) = fdr.cursor_row_index() {
+                    if !self.rows_view.in_view(cursor_row_index as u64) {
+                        fdr.reset_cursor();
+                    }
+                }
+
+                fdr.set_row_hint(self.rows_view.rows_from() as usize);
+            } else {
+                self.rows_view.set_filter(fdr).unwrap();
+            }
+        }
+
+        // update rows and elapsed time if there are new results
+        if self.show_stats {
+            self.csv_table_state.debug_stats.rows_view_elapsed(self.rows_view.elapsed());
+            if let Some(fdr) = &self.finder {
+                self.csv_table_state.debug_stats.finder_elapsed(fdr.elapsed());
+            }
+            else {
+                self.csv_table_state.debug_stats.finder_elapsed(None);
+            }
+        }
+
+        // TODO: is this update too late?
+        self.csv_table_state.set_rows_offset(self.rows_view.rows_from());
+        self.csv_table_state.selected = self.rows_view.selected();
+
+        if let Some(n) = self.rows_view.get_total_line_numbers() {
+            self.csv_table_state.set_total_line_number(n);
+        } else if let Some(n) = self.rows_view.get_total_line_numbers_approx() {
+            self.csv_table_state.set_total_line_number(n);
+        }
+
+        if let Some(f) = &self.finder {
+            // TODO: need to create a new finder every time?
+            self.csv_table_state.finder_state = FinderState::from_finder(f, &self.rows_view);
+        }
+
+        self.csv_table_state.user_error = self.user_error.clone();
+
+        //csv_table_state.debug = format!("{:?}", rows_view.rows_from());
+
+        terminal
+            .draw(|f| {
+                let size = f.size();
+
+                // TODO: check type of num_rows too big?
+                let frame_size_adjusted_num_rows =
+                    size.height.saturating_sub(self.num_rows_not_visible as u16) as u64;
+                self.rows_view
+                    .set_num_rows(frame_size_adjusted_num_rows)
+                    .unwrap();
+
+                let rows = self.rows_view.rows();
+                let csv_table = CsvTable::new(&self.headers, rows);
+
+                f.render_stateful_widget(csv_table, size, &mut self.csv_table_state);
+            })
+            .unwrap();
+
+        Ok(())
+    }
+}
+
 fn run_csvlens() -> Result<()> {
     let args = Args::parse();
 
@@ -156,190 +384,16 @@ fn run_csvlens() -> Result<()> {
     let file = SeekableFile::new(&args.filename)?;
     let filename = file.filename();
 
-    // Some lines are reserved for plotting headers (3 lines for headers + 2 lines for status bar)
-    let num_rows_not_visible = 5;
-
-    // Number of rows that are visible in the current frame
-    let num_rows = 50 - num_rows_not_visible;
-
-    let mut config = csv::CsvConfig::new(filename);
-    if let Some(d) = delimiter {
-        config.delimiter = d;
-    }
-    let shared_config = Arc::new(config);
-
-    let csvlens_reader = csv::CsvLensReader::new(shared_config.clone())
-        .context(format!("Failed to open file: {}", filename))?;
-    let mut rows_view = view::RowsView::new(csvlens_reader, num_rows)?;
-
-    let headers = rows_view.headers().clone();
-
     let stdout = io::stdout().into_raw_mode().unwrap();
     let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
 
-    let mut input_handler = InputHandler::new();
-    let mut csv_table_state = CsvTableState::new(args.filename, headers.len());
+    let mut app = App::new(
+        filename, delimiter, args.filename, show_stats,
+    ).context("Failed creating app")?;
 
-    let mut finder: Option<find::Finder> = None;
-    let mut first_found_scrolled = false;
-
-    let mut user_error: Option<String> = None;
-
-    loop {
-        terminal
-            .draw(|f| {
-                let size = f.size();
-
-                // TODO: check type of num_rows too big?
-                let frame_size_adjusted_num_rows =
-                    size.height.saturating_sub(num_rows_not_visible as u16) as u64;
-                rows_view
-                    .set_num_rows(frame_size_adjusted_num_rows)
-                    .unwrap();
-
-                let rows = rows_view.rows();
-                let csv_table = CsvTable::new(&headers, rows);
-
-                f.render_stateful_widget(csv_table, size, &mut csv_table_state);
-            })
-            .unwrap();
-
-        let control = input_handler.next();
-
-        // clear error message without changing other states on any action
-        if !matches!(control, Control::Nothing) {
-            user_error = None;
-        }
-
-        rows_view.handle_control(&control)?;
-
-        match &control {
-            Control::Quit => {
-                break;
-            }
-            Control::ScrollTo(_) => {
-                csv_table_state.reset_buffer();
-            }
-            Control::ScrollLeft => {
-                let new_cols_offset = csv_table_state.cols_offset.saturating_sub(1);
-                csv_table_state.set_cols_offset(new_cols_offset);
-            }
-            Control::ScrollRight => {
-                if csv_table_state.has_more_cols_to_show() {
-                    let new_cols_offset = csv_table_state.cols_offset.saturating_add(1);
-                    csv_table_state.set_cols_offset(new_cols_offset);
-                }
-            }
-            Control::ScrollToNextFound if !rows_view.is_filter() => {
-                if let Some(fdr) = finder.as_mut() {
-                    if let Some(found_record) = fdr.next() {
-                        scroll_to_found_record(found_record, &mut rows_view, &mut csv_table_state);
-                    }
-                }
-            }
-            Control::ScrollToPrevFound if !rows_view.is_filter() => {
-                if let Some(fdr) = finder.as_mut() {
-                    if let Some(found_record) = fdr.prev() {
-                        scroll_to_found_record(found_record, &mut rows_view, &mut csv_table_state);
-                    }
-                }
-            }
-            Control::Find(s) | Control::Filter(s) => {
-                let re = Regex::new(s.as_str());
-                if let Ok(target) = re {
-                    finder = Some(find::Finder::new(shared_config.clone(), target).unwrap());
-                    match control {
-                        Control::Find(_) => {
-                            // will scroll to first result below once ready
-                            first_found_scrolled = false;
-                            rows_view.reset_filter().unwrap();
-                        }
-                        Control::Filter(_) => {
-                            rows_view.set_rows_from(0).unwrap();
-                            rows_view.set_filter(finder.as_ref().unwrap()).unwrap();
-                        }
-                        _ => {}
-                    }
-                } else {
-                    finder = None;
-                    // TODO: how to show multi-line error
-                    user_error = Some(format!("Invalid regex: {}", s));
-                }
-                csv_table_state.reset_buffer();
-            }
-            Control::BufferContent(buf) => {
-                csv_table_state.set_buffer(input_handler.mode(), buf.as_str());
-            }
-            Control::BufferReset => {
-                csv_table_state.reset_buffer();
-                if finder.is_some() {
-                    finder = None;
-                    csv_table_state.finder_state = FinderState::FinderInactive;
-                    rows_view.reset_filter().unwrap();
-                }
-            }
-            _ => {}
-        }
-
-        if let Some(fdr) = finder.as_mut() {
-            if !rows_view.is_filter() {
-                // scroll to first result once ready
-                if !first_found_scrolled && fdr.count() > 0 {
-                    // set row_hint to 0 so that this always scrolls to first result
-                    fdr.set_row_hint(0);
-                    if let Some(found_record) = fdr.next() {
-                        scroll_to_found_record(found_record, &mut rows_view, &mut csv_table_state);
-                    }
-                    first_found_scrolled = true;
-                }
-
-                // reset cursor if out of view
-                if let Some(cursor_row_index) = fdr.cursor_row_index() {
-                    if !rows_view.in_view(cursor_row_index as u64) {
-                        fdr.reset_cursor();
-                    }
-                }
-
-                fdr.set_row_hint(rows_view.rows_from() as usize);
-            } else {
-                rows_view.set_filter(fdr).unwrap();
-            }
-        }
-
-        // update rows and elapsed time if there are new results
-        if show_stats {
-            csv_table_state.debug_stats.rows_view_elapsed(rows_view.elapsed());
-            if let Some(fdr) = &finder {
-                csv_table_state.debug_stats.finder_elapsed(fdr.elapsed());
-            }
-            else {
-                csv_table_state.debug_stats.finder_elapsed(None);
-            }
-        }
-
-        // TODO: is this update too late?
-        csv_table_state.set_rows_offset(rows_view.rows_from());
-        csv_table_state.selected = rows_view.selected();
-
-        if let Some(n) = rows_view.get_total_line_numbers() {
-            csv_table_state.set_total_line_number(n);
-        } else if let Some(n) = rows_view.get_total_line_numbers_approx() {
-            csv_table_state.set_total_line_number(n);
-        }
-
-        if let Some(f) = &finder {
-            // TODO: need to create a new finder every time?
-            csv_table_state.finder_state = FinderState::from_finder(f, &rows_view);
-        }
-
-        csv_table_state.user_error = user_error.clone();
-
-        //csv_table_state.debug = format!("{:?}", rows_view.rows_from());
-    }
-
-    Ok(())
+    app.main_loop(&mut terminal)
 }
 
 fn main() {
