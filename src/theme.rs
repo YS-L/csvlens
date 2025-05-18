32b50f312d863fefb7511f8692598989a71dddb8
Yung Siang Liau <liauys@gmail.com>
2025-05-19 03:05:03 +0800

    Detect terminal color scheme and add light theme

--- src/theme.rs
+++ src/theme.rs
@@ -1,4 +1,5 @@
 use ratatui::style::Color;
+use terminal_colorsaurus::{ColorScheme, QueryOptions, color_scheme};
 
 pub struct Theme {
     pub row_number: Color,
@@ -13,7 +14,15 @@ pub struct Theme {
 
 impl Theme {
     pub fn default() -> Self {
-        let gutter = Color::Rgb(128, 128, 128);
+        match color_scheme(QueryOptions::default()) {
+            Ok(ColorScheme::Dark) => Theme::dark(),
+            Ok(ColorScheme::Light) => Theme::light(),
+            _ => Theme::dark(),
+        }
+    }
+
+    pub fn dark() -> Self {
+        let gutter = Color::Rgb(131, 148, 150);
         Theme {
             row_number: gutter,
             border: gutter,
@@ -21,13 +30,33 @@ impl Theme {
             selected_bg: Color::Rgb(64, 64, 64),
             found: Color::Rgb(200, 0, 0),
             found_selected_bg: Color::LightYellow,
-            status: gutter,
+            status: Color::Rgb(128, 128, 128),
             column_colors: [
                 Color::Rgb(253, 151, 31),
                 Color::Rgb(102, 217, 239),
                 Color::Rgb(190, 132, 255),
                 Color::Rgb(249, 38, 114),
                 Color::Rgb(230, 219, 116),
+            ],
+        }
+    }
+
+    pub fn light() -> Self {
+        let gutter = Color::Rgb(131, 148, 150);
+        Theme {
+            row_number: gutter,
+            border: gutter,
+            selected_fg: Color::Rgb(192, 192, 192),
+            selected_bg: Color::Rgb(64, 64, 64),
+            found: Color::Rgb(200, 0, 0),
+            found_selected_bg: Color::LightYellow,
+            status: Color::Rgb(128, 128, 128),
+            column_colors: [
+                Color::Rgb(207, 112, 0),
+                Color::Rgb(0, 137, 179),
+                Color::Rgb(104, 77, 153),
+                Color::Rgb(249, 0, 90),
+                Color::Rgb(153, 143, 47),
             ],
         }
     }
